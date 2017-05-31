2b531f59575547c1425bef19c16263a63570b675
Dan Garcia <DanGx@outlook.com>
2017-05-31 01:43:19 +0000

    Lesson 39 No route matches    comments

---
+++ db/migrate/20170530221839_create_photos.rb
@@ -0,0 +1,10 @@
+class CreatePhotos < ActiveRecord::Migration[5.0]
+  def change
+    create_table :photos do |t|
+      t.text :caption
+      t.string :place_id
+      t.timestamps
+    end
+      add_index :photos, :place_id
+  end
+end
