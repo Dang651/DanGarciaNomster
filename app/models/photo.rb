91538917035eca2b68fa0976a825eed5f5a28e67
Dan Garcia <DanGx@outlook.com>
2017-06-03 04:22:11 +0000

    Lesson 39 Step 7

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,8 @@
-#class Photo < ApplicationRecord
 class Photo < ActiveRecord::Base
+#class Photo < ApplicationRecord
+  # require 'carrierwave/orm/activerecord'
   belongs_to :place
   mount_uploader :picture, PictureUploader
+  #mount_uploader :picture, ::PictureUploader
   #  mount_uploader :picture, Picture_Uploader
 end
