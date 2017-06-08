e979b96b0fb970bf6c5da878eea475110a5497a0
Dan Garcia <DanGx@outlook.com>
2017-06-08 02:16:04 +0000

    Lesson 41 actually sending email

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,7 +2,7 @@ class NotificationMailer < ApplicationMailer
   default from: "no-reply@nomsterapp.com"
   
   def comment_added
-    mail(to: "DanGx@outlook.com.com",
-          subject: "A comment hs been addedto your place")
+    mail(to: "DanGx@outlook.com",
+          subject: "A comment has been added to your place")
   end
 end
